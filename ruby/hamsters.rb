b24472f7fbb3e29b598d6dcf6652065ed21703e0
Carter McGee <carterm912@gmail.com>
2017-06-04 11:48:54 -0600

    create hamster survey.

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+
+puts "On a scale of 1 to 10, how loud is the hamster?"
+loud = gets.chomp
+
+puts "What color is the hamster?"
+color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? y/n"
+adopt = gets.chomp
+
+puts "How old do you think the hamster is?"
+	age = gets.chomp
+	if age == nil
+		age = nil
+end
