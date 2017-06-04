7b7a891d32f85695ffe321119de968393d64debf
Carter McGee <carterm912@gmail.com>
2017-06-04 15:04:11 -0600

    create basic questions and store answers.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born in?"
+birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? y/n"
+health = gets.chomp
