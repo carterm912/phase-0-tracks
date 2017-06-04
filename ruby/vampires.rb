4f5a04b6e1c01f651bf74331f871c55d6f614f08
Carter McGee <carterm912@gmail.com>
2017-06-04 15:28:29 -0600

    add detection for insurance and garlic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,7 +9,7 @@ puts "What year were you born in?"
 birth = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
-garlic = gets.chomp
+garlic = gets.chomp.to_s
 
 puts "Would you like to enroll in the company’s health insurance? y/n"
 health = gets.chomp
@@ -18,5 +18,21 @@ if 2017 - age == birth
 	puts "age is true"
 elsif 2017 - age != birth
 	puts "age is false"
-
 end	
+
+if garlic == "y"
+	like_garlic = true
+	puts "likes garlic"
+elsif garlic == "n"
+	like_garlic = false
+	puts "hate garlic"
+end
+
+if health == "y"
+	insurance = true
+	puts "health works"
+elsif health == "n"
+	insurance = false
+	puts "doesnt want health"
+end
+	
