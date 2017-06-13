86c3fe21d755e61f231c5f45349330af40dd0954
Carter McGee <carterm912@gmail.com>
2017-06-13 08:00:01 -0600

    fix struture of nested data, add calls.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,39 +2,38 @@
 # This is going to be a number of cars on a road with the cars having two different features (make and color) and having different occupants
 
 
+
 road = {
-	Car One: {
-		description: [
-			"Ford",
-			"Red"
-		]
-		occupants: [
-			"Tom"
-		]
-	}
+    car_one: {
+        make: 'Ford',
+        color: 'Red',
+        occupants: [
+            "Tom",
+            "Sally"
+        ]
+    },
+	car_two: {
+        make: 'Honda',
+        color: 'Blue',
+        occupants: [
+            "Suzan",
+            "Lindsey"
+        ]
+    },
+    car_three: {
+        make: 'Nissan',
+        color: 'Teal',
+        occupants: [
+            "Harry",
+            "Max"
+        ]
+    }
+}
 
-	'Car Two' {
-		description: [
-			"Nissan",
-			"Blue"
-		]
-		occupants: [
-			"Samantha",
-			"Suzan"
-		]
-	}
 
-	'Car Three' {
-		description: [
-			"Honda",
-			"Black"
-		]
-		occupants: [
-			"John",
-			"Sally"
-		]
-	}
 
+p road[:car_three][:occupants][0]
+p road[:car_one][:color]
 
 
 
