9ae557ff7d85c3876c34ef61d47bcc57f6c71185
Carter McGee <carterm912@gmail.com>
2017-06-13 07:44:26 -0600

    create 3 cars, gave them occupants and descriptions.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,41 @@
+#Nested Data Structures Release 1
+# This is going to be a number of cars on a road with the cars having two different features (make and color) and having different occupants
+
+
+road = {
+	Car One: {
+		description: [
+			"Ford",
+			"Red"
+		]
+		occupants: [
+			"Tom"
+		]
+	}
+
+	'Car Two' {
+		description: [
+			"Nissan",
+			"Blue"
+		]
+		occupants: [
+			"Samantha",
+			"Suzan"
+		]
+	}
+
+	'Car Three' {
+		description: [
+			"Honda",
+			"Black"
+		]
+		occupants: [
+			"John",
+			"Sally"
+		]
+	}
+
+
+
+
+
