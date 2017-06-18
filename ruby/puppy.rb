2e5933112121df52fa4b55514031ac67ae349302
Carter McGee <carterm912@gmail.com>
2017-06-18 14:53:56 -0600

    create new puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+# Puppy class layout
+# 
+# Species: Canis lupus familiaris
+#
+#Attributes:
+# Smart: Yes
+# Loyal: Yes
+# Adorable: Yes
+# User friendly
+#
+#Behavior:
+#Wag
+#Easily excited
+#Fetch
+#Run
+#
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
