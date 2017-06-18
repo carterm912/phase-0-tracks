e2105e18a9903f12a39dabb2f010e850d70e2556
Carter McGee <carterm912@gmail.com>
2017-06-18 14:55:40 -0600

    missed commit

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,22 @@
+class Todolist
+
+	def initializa(array)
+		@array = array
+	end
+
+	def get_items
+		p @array
+	end
+
+	def add_item(str)
+		@array << str
+	end
+
+	def delete_item(str)
+		@array.delete(str)
+	end
+
+	def get_item(index)
+		@array[index]
+	end
+end
