63f7b13c4688012ca70e2bbfc9f198cc831f683c
Carter McGee <carterm912@gmail.com>
2017-06-18 15:25:16 -0600

    complete release 0, create santa class with speak, cookie, and inititialize methods.

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+class Santa
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		p "That was a good #{cookie_type}"
+	end
+
+	def initialize
+		p "initializing Santa instance ..."
+	end
+
+
+
+end
