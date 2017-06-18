822ad7641d6e8fdc5d5ff4a89914a5f0847ecde3
Carter McGee <carterm912@gmail.com>
2017-06-18 15:08:04 -0600

    fixing merge conflict

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
-=======
-11:13 AM
->>>>>>> 1baa5784b3089bb11c7c7558d7c3ba9f53886049
 class Puppy
 
   def fetch(toy)
@@ -26,39 +22,9 @@ def initialize
  p "Initializing new puppy instance..."
 
 end
-<<<<<<< HEAD
 
 end
 
-puppy = Puppy.new
-
-puppy.fetch("rope")
-puppy.speak(5)
-puppy.roll_over
-puppy.dog_years(3)
-
-
-=======
->>>>>>> 1baa5784b3089bb11c7c7558d7c3ba9f53886049
-
-cat_number = 0
-cat_store = []
-class Cat
-
-def initialize
-p "Cat spawn"
-
-
-end
-
-def speak
-
-p "meow"
-end
-end
-
-<<<<<<< HEAD
-=======
 puppy = Puppy.new
 
 puppy.fetch("rope")
@@ -84,7 +50,6 @@ p "meow"
 end
 end
 
->>>>>>> 1baa5784b3089bb11c7c7558d7c3ba9f53886049
 until cat_number == 50
 cat = Cat.new
 cat_number += 1
