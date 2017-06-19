81743c7c45ec969c7d7c2918177fe5524611616f
Carter McGee <carterm912@gmail.com>
2017-06-18 21:07:08 -0600

    add yell_happily method and called both self methods correctly.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-	#we'll put some methods here soon.
-end
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+ 	end
+
+ 	def self.yell_happily(words)
+ 		words + "!" + " :D"
+ 	end
+
+ end
+
+ p Shout.yell_angrily("HIYAAAAA")
+ p Shout.yell_happily("hiyaaa")
